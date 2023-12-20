5f5ecc9918a9a2a441b668a74d9243ea9d5bc8de
dstartsev <dstartsev@profitbase.ru>
2023-12-20 19:48:44 +0500

    test env

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,25 +2,25 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Entity\VacanciesParseQuery;
-use App\Repository\UserRepository;
-use App\Repository\VacanciesParseQueryRepository;
-use App\Service\GetIndustriesService;
-use App\Service\ParserService\AdapterService;
-use App\Service\ParserService\ParserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-
+    #[Route('/api/test', name: 'test')]
+    public function apiTest()
+    {
+        return new Response(
+            '<html><body>HelloApiWorld</body></html>'
+        );
+    }
 
     #[Route('/', name: 'index')]
-    public function index(
-    )
+    public function index()
     {
-        return $this->redirectToRoute('app_login');
+        return new Response(
+            '<html><body>HelloApiWorld</body></html>'
+        );
     }
 }
